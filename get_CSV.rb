2c262e2d78e23def21da3e1078f233673401a8b8
David Sackmary <david-sackmary@users.noreply.github.com>
2014-12-18 11:52:06 -0800

    Update get_CSV.rb
    oops... i fixed it in a day...

--- get_CSV.rb
+++ get_CSV.rb
@@ -1,33 +1,35 @@
 require 'csv'
 require 'optparse'
 
+#input text file of CSV entries
+#input 2014-redhat.xml
+#output patches
+
 if ARGV.length != 3
-  abort "usage: hi.rb input.csv output.csv output2.csv"
+  abort "usage: get_CSV.rb input.csv redhat-2014.xml output.csv"
 end
 
 #Print beginning time
 time1 = Time.new
 puts "Current Time : " + time1.inspect
 
-input_csv  = File.open(ARGV[0],"r")
-output1_csv = File.open(ARGV[1],"w+")
-output2_csv = File.open(ARGV[2],"w+")
-
-#packages = "grep iptables iputils libyaml lua libX11 nano readline rpm yum"
-cve = "CVE-2014-4877"
+in1_csv  = File.open(ARGV[0],"r")
+in2_csv  = File.open(ARGV[1],"r")
+out_csv = File.open(ARGV[2],"w+")
 
 #Search our current CSV for matching criteria and output the patch.
-CSV.foreach(input_csv) do |row|       
-    #match CVE
-    puts cve
-    if row[5] == cve
-          output1_csv.write(row.to_csv)   
+CSV.foreach(in1_csv) do |row|
+  CSV.foreach(in2_csv) do |line|
+    if line[5] == row[0]  
+      puts line.to_csv
+      out_csv.write(line.to_csv)
     end
+  end
 end
 
-input_csv.close()
-output1_csv.close()
-output2_csv.close()
+in1_csv.close()
+in2_csv.close()
+out_csv.close()
 
 #Print ending time
 time1 = Time.new
