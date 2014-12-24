6f533594d69e57683ff7e4bfc466751eb5c355f2
David Sackmary <david-sackmary@users.noreply.github.com>
2014-12-24 08:17:27 -0800

    Update get_minfix.rb

--- get_minfix.rb
+++ get_minfix.rb
@@ -1,1 +1,8 @@
 #Given an input of packages-on-servers-I-care-about, provide minfixes for vulnerabilities.
+# 
+# advice request:
+Dave ~ I am about to code a basic vulnerability scanner.  Is there, instead, something more useful/valuable I could be coding?
+My focus is entirely on building skillset and reputation.  both of which are starting from git 0.
+
+Happy Holidays,
+DSS
